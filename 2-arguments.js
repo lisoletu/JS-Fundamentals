e98428521cc60d40aedb4ea2f693ef8d6dccb066
Lisoletu Digala <lisoletudigala@gmail.com>
2025-07-16 19:46:27 +0200

    update 2-arguments.js

--- 2-arguments.js
+++ 2-arguments.js
@@ -3,7 +3,7 @@ const name= 'lisoletuuuu';
 if (name.length >= 10) {
   console.log('No argument');
 }
-else if (name.length >= 5) {
+else if (name.length >= 8 || name.includes('e')) {
   console.log('Argument found');
 } else {
   console.log('Arguments found');
