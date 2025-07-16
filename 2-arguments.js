917c179f04c734617466b5fcfa69b2e58af73237
Lisoletu Digala <lisoletudigala@gmail.com>
2025-07-16 19:05:34 +0200

    update 2-arguments.js

--- 2-arguments.js
+++ 2-arguments.js
@@ -1,9 +1,22 @@
-const name= 'lisoletu';
+const name= 'lisoletuuu';
 
-if (name.length >=10){
-  console.log ('No argument');
-}else if (name.length>=5){
-  console.log ('Argument found');
+/*if (name.length >= 10) {
+  console.log('No argument');
+} else if (name.length >= 5) {
+  console.log('Argument found');
 } else {
-  console.log ('Argument found')
-};
+  console.log('Arguments found');
+}*/
+
+if (name.length>=3){
+  console.log('No argument');
+}
+
+
+if (name.length >= 10) {
+  console.log('Argument found');
+}
+
+if (name.length == 10) {
+  console.log('Argument found');
+} 
