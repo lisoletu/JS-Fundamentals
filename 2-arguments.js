8a9b39f0c8211065471bcb9f34b84418e0a0dd30
Lisoletu Digala <lisoletudigala@gmail.com>
2025-07-16 18:32:36 +0200

    Add 2-arguments.js

---
+++ 2-arguments.js
@@ -0,0 +1,9 @@
+const name= 'lisoletu';
+
+if (name.length >=10){
+  console.log ('No argument');
+}else if (name.length>=5){
+  console.log ('Argument found');
+} else {
+  console.log ('Argument found')
+};
