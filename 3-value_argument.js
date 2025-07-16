aeb3d5d5f8b803aec25e74e13d7181118a903af7
Lisoletu Digala <lisoletudigala@gmail.com>
2025-07-16 22:05:44 +0200

    Update 3-value_argument.js

--- 3-value_argument.js
+++ 3-value_argument.js
@@ -1,4 +1,22 @@
-function name(){
+/*const name = ['lina', 'lisa']
+
+if (name === undefined){
     console.log ('No argument');
-}
+}else{
+    console.log (name);
+};*/
+
+
+
+
+
+
+
+
+const name= function(){
+    console.log ('No argument');
+    console.log (`$ {name}`);
+};
 name();
+
+
