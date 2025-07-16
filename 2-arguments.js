54117873349af0a5718f291403e40d0454e75ec6
lisoletu <163672027+lisoletu@users.noreply.github.com>
2025-07-17 00:17:46 +0200

    Update 2-arguments.js
    another update

--- 2-arguments.js
+++ 2-arguments.js
@@ -1,17 +1,16 @@
-/*const name= ['lisoletu', 'lina']
-
+/*const name='liso'
+const count = liso.length
 if (name.length === 0) {
   console.log('No argument');
 };
-
-
-/*else if (name.length === 1) {
+else if (name.length === 1) {
   console.log('Argument found');
 } else {
-  console.log('Arguments found');
-};*/
+  console.log('Argument found');
+};
+name();*/
 
-let input = prompt ('Enter arguments separated by spaces:');
+/*let input = prompt ('Enter arguments separated by spaces:');
 let args = input ? input.trim().split(" "): [];
 
 if (args.length===0 || args[0] === " "){
@@ -21,5 +20,17 @@ if (args.length===0 || args[0] === " "){
   console.log ('Argument found');
 } else {
   console.log ('Argument found')
-}; 
-
+}; */
+function noofarguments(...args){
+  const count = args.length; 
+  if (count.length === 0) {
+  console.log('No argument');
+} else if (count.length === 1) {
+  console.log('Argument found', args[0]);
+} else {
+  console.log(`${count} Argument found `);
+};
+}
+noofarguments(3, 5, 'lina'); 
+noofarguments('pear');
+noofarguments();
